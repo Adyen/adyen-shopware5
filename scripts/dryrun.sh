4b892d791d060ae9c9a96c7c1d5ebd1cb09134f0
Bert Verbessem <bert.verbessem@phpro.be>
2019-09-02 10:47:27 +0200

    update pipelines

--- scripts/dryrun.sh
+++ scripts/dryrun.sh
@@ -3,5 +3,5 @@ set -e
 
 echo "Getting differences through rsync"
 cd "$WORKSPACE" || exit
-rsync -avzphlcn --delete --exclude-from="scripts/excludes" --chmod=u+rwx,g+rwx,o-rx "src/" "$SSHUSER@$PROJECTHOST:$PROJECT_ROOT"
+rsync -avzphlcn --delete --exclude-from="scripts/excludes" --chmod=u+rwx,g+rwx,o-rx "." "$SSHUSER@$PROJECTHOST:$PROJECT_ROOT"
 echo "All Done!"
--- scripts/prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Updating using composer"
-cd "$WORKSPACE/src" && /usr/bin/php7.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
-
