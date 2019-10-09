79a53d62dfb5a49d172d1e031544562405c0afd5
Wanne Van Camp <wanne.vancamp@phpro.be>
2019-10-09 13:32:19 +0200

    ASW-0 fix composer

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Updating using composer"
-cd "$WORKSPACE/src" && /usr/bin/php7.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
+cd "$WORKSPACE" && /usr/bin/php7.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
 
