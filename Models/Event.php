b916d2b22c3432cd01e6aaad9678ebcac92281b4
Rune Laenen <rune.laenen@phpro.be>
2019-10-29 15:40:16 +0100

    ASW-82 Add chargeback events

--- Models/Event.php
+++ Models/Event.php
@@ -11,8 +11,6 @@ class Event
     const NOTIFICATION_RECEIVE = 'MeteorAdyen_Notification_onReceive_json';
     const NOTIFICATION_SAVE_FILTER_NOTIFICATIONS = 'MeteorAdyen_Notification_saveNotifications_notifications';
     const NOTIFICATION_ON_SAVE_NOTIFICATIONS = 'MeteorAdyen_Notification_saveNotifications_onSave';
-    const NOTIFICATION_FIND_HANDLERS = 'MeteorAdyen_Notification_FindHandlers';
-    const NOTIFICATION_PROCESS = 'MeteorAdyen_Notification_Process';
     const NOTIFICATION_PROCESS_AUTHORISATION = 'MeteorAdyen_Notification_Process_Authorisation';
     const NOTIFICATION_PROCESS_CANCELLATION = 'MeteorAdyen_Notification_Process_Cancellation';
     const NOTIFICATION_PROCESS_CAPTURE = 'MeteorAdyen_Notification_Process_Capture';
@@ -20,4 +18,6 @@ class Event
     const NOTIFICATION_PROCESS_REFUND = 'MeteorAdyen_Notification_Process_Refund';
     const NOTIFICATION_PROCESS_REFUND_FAILED = 'MeteorAdyen_Notification_Process_RefundFailed';
     const NOTIFICATION_PROCESS_REFUNDED_REVERSED = 'MeteorAdyen_Notification_Process_RefundedReversed';
+    const NOTIFICATION_PROCESS_CHARGEBACK = 'MeteorAdyen_Notification_Process_Chargeback';
+    const NOTIFICATION_PROCESS_CHARGEBACK_REVERSED = 'MeteorAdyen_Notification_Process_ChargebackReversed';
 }
