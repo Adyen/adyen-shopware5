555f4fadf1464da72925814d701ef06c11dabf82
Wanne Van Camp <wanne.vancamp@phpro.be>
2020-03-02 21:09:01 +0100

    ASW-0 fix api test

--- Controllers/Backend/TestAdyenApi.php
+++ Controllers/Backend/TestAdyenApi.php
@@ -9,19 +9,19 @@ class Shopware_Controllers_Backend_TestAdyenApi extends Shopware_Controllers_Bac
         $paymentMethodService = $this->get('meteor_adyen.components.adyen.payment.method');
         $configuration = $this->get('meteor_adyen.components.configuration');
 
-        try {
-            if (empty($configuration->getApiKey()) || empty($configuration->getMerchantAccount())) {
-                $this->response->setHttpResponseCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-                $this->View()->assign('responseText',
-                    "Missing API configuration. Save the configuration form before testing");
-            } else {
-                $paymentMethodService->getPaymentMethods('BE', 'EUR', 20, false);
-                $this->View()->assign('responseText', 'Adyen API connected');
-            }
-        } catch (Exception $e) {
-            $this->get('meteor_adyen.logger')->error($e);
+        $responseText = 'Adyen API failed, check error logs';
+        $this->response->setHttpResponseCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+
+        if (empty($configuration->getApiKey()) || empty($configuration->getMerchantAccount())) {
             $this->response->setHttpResponseCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-            $this->View()->assign('responseText', $e->getMessage());
+            $responseText = 'Missing API configuration. Save the configuration form before testing';
         }
+
+        if (!empty($paymentMethodService->getPaymentMethods('BE', 'EUR', 20, false))) {
+            $this->response->setHttpResponseCode(Response::HTTP_OK);
+            $responseText = 'Adyen API connected';
+        }
+
+        $this->View()->assign('responseText', $responseText);
     }
 }
