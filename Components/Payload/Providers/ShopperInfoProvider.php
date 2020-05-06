441b980cd4076a35efbafd37bddae3e8860fabf1
Wanne Van Camp <wanne.vancamp@phpro.be>
2020-05-06 16:15:00 +0200

    ASW-183 add orderlines for Klarna payment

--- Components/Payload/Providers/ShopperInfoProvider.php
+++ Components/Payload/Providers/ShopperInfoProvider.php
@@ -18,7 +18,23 @@ class ShopperInfoProvider implements PaymentPayloadProvider
     public function provide(PaymentContext $context): array
     {
         return [
-            'shopperIP' => $context->getShopperInfo()['shopperIP']
+            'shopperIP' => $context->getShopperInfo()['shopperIP'],
+            'shopperEmail' => $context->getOrder()->getCustomer()->getEmail(),
+            'shopperName' => [
+                'firstName' => $context->getOrder()->getCustomer()->getFirstname(),
+                'lastName' => $context->getOrder()->getCustomer()->getLastname(),
+                'gender' => $context->getOrder()->getCustomer()->getSalutation(),
+            ],
+            'shopperLocale' => Shopware()->Shop()->getLocale()->getLocale(),
+            'shopperReference' => $context->getOrder()->getCustomer()->getId(),
+            'countryCode' => $context->getOrder()->getBilling()->getCountry()->getIso(),
+            'billingAddress' => [
+                'city' => $context->getOrder()->getBilling()->getCity(),
+                'country' => $context->getOrder()->getBilling()->getCountry()->getIso(),
+                'houseNumberOrName' => $context->getOrder()->getBilling()->getNumber(),
+                'postCode' => $context->getOrder()->getBilling()->getZipCode(),
+                'street' => $context->getOrder()->getBilling()->getStreet(),
+            ],
         ];
     }
 }
