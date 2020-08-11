d54d36d11ee55ecc7d09096d0eadbc1a79ed08ab
Rune Laenen <rune.laenen@phpro.be>
2020-08-11 12:05:12 +0200

    Fix #23 PW-2747 Improve exception logging

--- Components/Adyen/PaymentMethodService.php
+++ Components/Adyen/PaymentMethodService.php
@@ -88,7 +88,14 @@ class PaymentMethodService
         try {
             $paymentMethods = $checkout->paymentMethods($requestParams);
         } catch (AdyenException $e) {
-            $this->logger->critical($e);
+            $this->logger->critical('Adyen Exception', [
+				'message' => $e->getMessage(),
+            	'file' => $e->getFile(),
+				'line' => $e->getLine(),
+				'statuc' => $e->getStatus(),
+				'errorType' => $e->getErrorType(),
+				'status' => $e->getStatus()
+			]);
             return [];
         }
 
