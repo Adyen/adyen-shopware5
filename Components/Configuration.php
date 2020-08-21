af817c6c2e30cf1e0db2f69dd6d99ca75d1451ae
Rune Laenen <rune.laenen@phpro.be>
2020-08-21 09:37:04 +0200

    PW-2505 Distinquish fields of test and live

--- Components/Configuration.php
+++ Components/Configuration.php
@@ -46,13 +46,18 @@ class Configuration
      * @param bool $shop
      * @return string
      */
-    public function getEnvironment($shop = false): string
+    public function getEnvironment($shop = false, $lowercase = false): string
     {
+    	$environment = Environment::TEST;
         if ($this->getConfig('environment', $shop) === self::ENV_LIVE) {
-            return Environment::LIVE;
+			$environment = Environment::LIVE;
         }
 
-        return Environment::TEST;
+        if ($lowercase) {
+        	return strtolower($environment);
+		}
+
+        return $environment;
     }
 
     /**
@@ -84,6 +89,8 @@ class Configuration
             $shop = null;
         }
 
+        Shopware()->Container()->get('corelogger')->error($key);
+
         $config = $this->cachedConfigReader->getByPluginName(AdyenPayment::NAME, $shop);
 
         if ($key === null) {
@@ -103,7 +110,10 @@ class Configuration
      */
     public function getApiKey($shop = false): string
     {
-        return (string)$this->getConfig('api_key', $shop);
+        return (string)$this->getConfig(
+        	'api_key_' . $this->getEnvironment($shop, true),
+			$shop
+		);
     }
 
     /**
@@ -130,7 +140,10 @@ class Configuration
      */
     public function getNotificationHmac($shop = false): string
     {
-        return (string)$this->getConfig('notification_hmac', $shop);
+        return (string)$this->getConfig(
+        	'notification_hmac_' . $this->getEnvironment($shop, true),
+			$shop
+		);
     }
 
     /**
@@ -139,7 +152,10 @@ class Configuration
      */
     public function getNotificationAuthUsername($shop = false): string
     {
-        return (string)$this->getConfig('notification_auth_username', $shop);
+        return (string)$this->getConfig(
+        	'notification_auth_username_' . $this->getEnvironment($shop, true),
+			$shop
+		);
     }
 
     /**
@@ -148,7 +164,10 @@ class Configuration
      */
     public function getNotificationAuthPassword($shop = false): string
     {
-        return (string)$this->getConfig('notification_auth_password', $shop);
+        return (string)$this->getConfig(
+        	'notification_auth_password_' . $this->getEnvironment($shop, true),
+			$shop
+		);
     }
 
     /**
