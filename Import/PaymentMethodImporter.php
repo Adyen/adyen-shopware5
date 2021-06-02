42c5475654ede08c29e772d8b271dfdc1a3cf476
Filippe Bortels <filippe.bortels@phpro.be>
2021-06-02 11:49:45 +0200

    AP-279 importer structure

---
+++ Import/PaymentMethodImporter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Import;
+
+class PaymentMethodImporter implements PaymentMethodImporterInterface
+{
+    public function __invoke()
+    {
+        // TODO actual importer
+    }
+}
---
+++ Import/PaymentMethodImporterInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Import;
+
+interface PaymentMethodImporterInterface
+{
+    public function __invoke();
+}
