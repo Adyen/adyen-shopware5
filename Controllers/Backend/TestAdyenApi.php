5bea57712eea239c0359ec88485a9095e8e786fe
davebueds <david.bueds@meteor.be>
2021-06-15 15:21:25 +0200

    ASW-288 use refactored rules

--- Controllers/Backend/TestAdyenApi.php
+++ Controllers/Backend/TestAdyenApi.php
@@ -13,16 +13,16 @@ class Shopware_Controllers_Backend_TestAdyenApi extends Shopware_Controllers_Bac
      */
     private $apiConfigValidator;
     /**
-     * @var UsedMainShopConfigRule
+     * @var \AdyenPayment\Rule\AdyenApi\UsedFallbackConfigRule
      */
-    private $usedMainShopConfigRule;
+    private $usedFallbackConfigRule;
 
     public function preDispatch()
     {
         parent::preDispatch();
 
         $this->apiConfigValidator = $this->get('AdyenPayment\Components\Adyen\ApiConfigValidator');
-        $this->usedMainShopConfigRule = $this->get('AdyenPayment\Rule\AdyenApi\UsedMainShopConfigRule');
+        $this->usedFallbackConfigRule = $this->get('AdyenPayment\Rule\AdyenApi\UsedFallbackConfigRule');
     }
 
     public function runAction()
@@ -41,7 +41,7 @@ class Shopware_Controllers_Backend_TestAdyenApi extends Shopware_Controllers_Bac
             return;
         }
 
-        $usedFallback = (1 !== $shopId && ($this->usedMainShopConfigRule)($shopId));
+        $usedFallback = ($this->usedFallbackConfigRule)($shopId);
         $this->response->setHttpResponseCode(Response::HTTP_OK);
         $this->View()->assign('responseText', sprintf(
             '%sAdyen API connection successful.',
