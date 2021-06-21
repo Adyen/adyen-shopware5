645cc85f18f8b7e1dd4494a36ed89dac49bdd205
davebueds <david.bueds@meteor.be>
2021-06-21 17:09:42 +0200

    AP-279 final import payment method command

--- Commands/ImportPaymentMethodsCommand.php
+++ Commands/ImportPaymentMethodsCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class ImportPaymentMethodsCommand extends ShopwareCommand
+final class ImportPaymentMethodsCommand extends ShopwareCommand
 {
     /**
      * @var PaymentMethodImporterInterface
