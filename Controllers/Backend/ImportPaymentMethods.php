507f67680a4273e10bc1ffe0eb47114ddc42b239
davebueds <david.bueds@meteor.be>
2021-06-24 16:29:18 +0200

    AP-290 backend import button logs and imports

--- Controllers/Backend/ImportPaymentMethods.php
+++ Controllers/Backend/ImportPaymentMethods.php
@@ -4,47 +4,48 @@ declare(strict_types=1);
 
 //phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps, Generic.Files.LineLength.TooLong
 use AdyenPayment\Import\PaymentMethodImporterInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class Shopware_Controllers_Backend_ImportPaymentMethods extends Shopware_Controllers_Backend_ExtJs
 {
     /** @var PaymentMethodImporterInterface */
     private $paymentMethodImporter;
+    /** @var LoggerInterface */
+    private $logger;
 
     public function preDispatch()
     {
         parent::preDispatch();
 
         $this->paymentMethodImporter = $this->get('AdyenPayment\Import\PaymentMethodImporter');
+        $this->logger = $this->get('adyen_payment.logger');
     }
 
     public function importAction()
     {
         try {
-//            $count = 1;
-//            $this->View()->assign(
-//                'responseText',
-//                sprintf('Successfully imported %s payment method(s)',
-//                $count
-//                )
-//            );
-            $counter = count(iterator_to_array(
-                $this->paymentMethodImporter->__invoke()
-            ));
+            $counter = 0;
+
+            foreach ($this->paymentMethodImporter->importAll() as $importResult) {
+                ++$counter;
+            }
 
             $this->response->setHttpResponseCode(Response::HTTP_OK);
-            $this->View()->assign(
-                'responseText',
-                sprintf(
-                    'Successfully imported payment method(s)'
-                )
-            );
+            $this->View()->assign('responseText', sprintf(
+                'Imported successfully %s payment method(s)',
+                $counter
+            ));
         } catch (Exception $e)
         {
+            $this->logger->error($e->getMessage(), [
+                'trace' => $e->getTraceAsString(),
+            ]);
+
             $this->View()->assign(
                 'responseText',
                 sprintf(
-                    'It fails'
+                    'Import of payment methods failed.'
                 )
             );
         }
