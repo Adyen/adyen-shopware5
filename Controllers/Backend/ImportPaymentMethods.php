780bc3f56af97a4aa48bf6a92d5b651fe318aa32
davebueds <david.bueds@meteor.be>
2021-06-24 13:12:51 +0200

    AP-290 WIP

--- Controllers/Backend/ImportPaymentMethods.php
+++ Controllers/Backend/ImportPaymentMethods.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 //phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace, Squiz.Classes.ValidClassName.NotCamelCaps, Generic.Files.LineLength.TooLong
-use AdyenPayment\Import\PaymentMethodImporter;
+use AdyenPayment\Import\PaymentMethodImporterInterface;
 use Symfony\Component\HttpFoundation\Response;
 
 class Shopware_Controllers_Backend_ImportPaymentMethods extends Shopware_Controllers_Backend_ExtJs
 {
-    /** @var PaymentMethodImporter */
+    /** @var PaymentMethodImporterInterface */
     private $paymentMethodImporter;
 
     public function preDispatch()
@@ -20,21 +20,34 @@ class Shopware_Controllers_Backend_ImportPaymentMethods extends Shopware_Control
 
     public function importAction()
     {
-        $this->View()->assign(
-            'responseText',
-            sprintf('Successfully imported %s payment method(s)', 1)
-        );
-//        $counter = 0;
-//
-//        foreach ($this->paymentMethodImporter->__invoke() as $importPaymentMethod)
-//        {
-//            ++$counter;
-//        }
-//
-//        $this->response->setHttpResponseCode(Response::HTTP_OK);
-//        $this->View()->assign(
-//            'responseText',
-//            sprintf('Successfully imported %s payment method(s)', $counter)
-//        );
+        try {
+//            $count = 1;
+//            $this->View()->assign(
+//                'responseText',
+//                sprintf('Successfully imported %s payment method(s)',
+//                $count
+//                )
+//            );
+            $counter = count(iterator_to_array(
+                $this->paymentMethodImporter->__invoke()
+            ));
+
+            $this->response->setHttpResponseCode(Response::HTTP_OK);
+            $this->View()->assign(
+                'responseText',
+                sprintf(
+                    'Successfully imported payment method(s)'
+                )
+            );
+        } catch (Exception $e)
+        {
+            $this->View()->assign(
+                'responseText',
+                sprintf(
+                    'It fails'
+                )
+            );
+        }
+
     }
 }
