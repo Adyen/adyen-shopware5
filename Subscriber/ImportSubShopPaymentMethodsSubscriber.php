2c3e3c95794ae96369fbe28919745e7be4eeb183
davebueds <david.bueds@meteor.be>
2021-07-13 17:57:25 +0200

    AP-291 add ImportSubShopPaymentMethodsSubscriber.php

---
+++ Subscriber/ImportSubShopPaymentMethodsSubscriber.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Subscriber;
+
+use AdyenPayment\Dbal\Writer\Payment\PaymentMeanWriterInterface;
+use AdyenPayment\Import\PaymentMethodImporterInterface;
+use AdyenPayment\Rule\AdyenApi\MainShopConfigRule;
+use Doctrine\Common\Persistence\ObjectRepository;
+use Enlight\Event\SubscriberInterface;
+use Shopware\Models\Shop\Shop;
+use Symfony\Component\HttpFoundation\Response;
+
+final class ImportSubShopPaymentMethodsSubscriber implements SubscriberInterface
+{
+    const SAVE_VALUES_ACTION = 'saveValues';
+
+    /**
+     * @var ObjectRepository
+     */
+    private $shopRepository;
+    /**
+     * @var MainShopConfigRule
+     */
+    private $mainShopConfigRuleChain;
+    /**
+     * @var PaymentMeanWriterInterface
+     */
+    private $paymentMeanWriter;
+    /**
+     * @var PaymentMethodImporterInterface
+     */
+    private $paymentMethodImporter;
+
+    public function __construct(
+        ObjectRepository $shopRepository,
+        MainShopConfigRule $mainShopConfigRule,
+        PaymentMeanWriterInterface $paymentMeanWriter,
+        PaymentMethodImporterInterface $paymentMethodImporter
+    )
+    {
+        $this->shopRepository = $shopRepository;
+        $this->mainShopConfigRuleChain = $mainShopConfigRule;
+        $this->paymentMeanWriter = $paymentMeanWriter;
+        $this->paymentMethodImporter = $paymentMethodImporter;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatch_Backend_Config' => '__invoke',
+        ];
+    }
+
+    public function __invoke(\Enlight_Event_EventArgs $args)
+    {
+        if (!$this->isNewSubShopAdded($args->get('request')->getParam('id'), $args->get('response'), $args->get('request')->getActionName())) {
+            return;
+        }
+
+        /** @var Shop $shop */
+        $shop = $this->shopRepository->findBy([], ['id' => 'desc'], 1)[0];
+        $mainShop = $this->shopRepository->find(1);
+
+        if (($this->mainShopConfigRuleChain)($shop, $mainShop)) {
+            $this->paymentMeanWriter->updateAdyenPaymentMethodBySubshopId($shop->getId());
+            return;
+        }
+
+        $this->paymentMethodImporter->importForShop($shop);
+    }
+
+    private function isNewSubShopAdded($id, $response, string $action): bool
+    {
+        return (null === $id)
+            && (Response::HTTP_OK === $response->getHttpResponseCode())
+            && ($this->isSaveValuesActionTriggered($action));
+    }
+
+    private function isSaveValuesActionTriggered(string $action): bool
+    {
+        return $this::SAVE_VALUES_ACTION === $action ?? false;
+    }
+}
