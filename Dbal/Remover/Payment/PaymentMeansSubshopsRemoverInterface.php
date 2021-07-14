71d55e85b4be2af2bc15b02ce2183b244b85f320
davebueds <david.bueds@meteor.be>
2021-07-14 13:55:13 +0200

    AP-291 remove records on paymentmeans_subshops when removing subshop

---
+++ Dbal/Remover/Payment/PaymentMeansSubshopsRemoverInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Dbal\Remover\Payment;
+
+interface PaymentMeansSubshopsRemoverInterface
+{
+    public function deleteAdyenPaymentMethodsForSubshop(int $subshopId);
+}
