ddbfe82d0bc76e555af931f1a9d6d5c58fd50fbf
Filippe Bortels <filippe.bortels@phpro.be>
2021-10-15 16:09:44 +0200

    ASW-359 GooglePay config provider

---
+++ Components/WebComponents/ConfigContext.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Components\WebComponents;
+
+final class ConfigContext
+{
+    /**
+     * @var array
+     */
+    private $userData;
+    /**
+     * @var array
+     */
+    private $basket;
+
+    private function __construct()
+    {
+    }
+
+    public static function fromCheckoutEvent(\Enlight_Event_EventArgs $args):self
+    {
+        /* @var $subject \Enlight_Controller_Action */
+        $subject = $args->getSubject();
+        $userData = $subject->View()->getAssign('sUserData') ?? [];
+        $basket = $subject->View()->getAssign('sBasket') ?? [];
+
+        $new = new self();
+        $new->userData = $userData;
+        $new->basket = $basket;
+
+        return $new;
+    }
+
+    public function getUserData(): array
+    {
+        return $this->userData;
+    }
+
+    public function getBasket(): array
+    {
+        return $this->basket;
+    }
+}
