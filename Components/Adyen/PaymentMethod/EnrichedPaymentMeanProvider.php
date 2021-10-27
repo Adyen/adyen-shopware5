fca81ad147b2db9243a492fb4c32db0b5c92c488
Filippe Bortels <filippe.bortels@phpro.be>
2021-10-27 12:27:15 +0200

    ASW-0 cleanup code negligence

--- Components/Adyen/PaymentMethod/EnrichedPaymentMeanProvider.php
+++ Components/Adyen/PaymentMethod/EnrichedPaymentMeanProvider.php
@@ -8,46 +8,22 @@ use AdyenPayment\Collection\Payment\PaymentMeanCollection;
 use AdyenPayment\Collection\Payment\PaymentMethodCollection;
 use AdyenPayment\Components\Adyen\Builder\PaymentMethodOptionsBuilderInterface;
 use AdyenPayment\Components\Adyen\PaymentMethodService;
-use AdyenPayment\Doctrine\Writer\PaymentMethodWriterInterface;
 use AdyenPayment\Enricher\Payment\PaymentMethodEnricherInterface;
-use AdyenPayment\Models\Payment\PaymentMethodType;
-use Doctrine\Persistence\ObjectRepository;
 
 final class EnrichedPaymentMeanProvider implements EnrichedPaymentMeanProviderInterface
 {
-    /**
-     * @var PaymentMethodService
-     */
-    protected $paymentMethodService;
-    /**
-     * @var PaymentMethodOptionsBuilderInterface
-     */
-    private $paymentMethodOptionsBuilder;
-    /**
-     * @var PaymentMethodEnricherInterface
-     */
-    private $paymentMethodEnricher;
-    /**
-     * @var PaymentMethodWriterInterface
-     */
-    private $paymentMethodWriter;
-    /**
-     * @var ObjectRepository
-     */
-    private $shopRepository;
+    private PaymentMethodService $paymentMethodService;
+    private PaymentMethodOptionsBuilderInterface $paymentMethodOptionsBuilder;
+    private PaymentMethodEnricherInterface $paymentMethodEnricher;
 
     public function __construct(
         PaymentMethodService $paymentMethodService,
         PaymentMethodOptionsBuilderInterface $paymentMethodOptionsBuilder,
         PaymentMethodEnricherInterface $paymentMethodEnricher,
-        PaymentMethodWriterInterface $paymentMethodWriter,
-        ObjectRepository $shopRepository
     ) {
         $this->paymentMethodService = $paymentMethodService;
         $this->paymentMethodOptionsBuilder = $paymentMethodOptionsBuilder;
         $this->paymentMethodEnricher = $paymentMethodEnricher;
-        $this->paymentMethodWriter = $paymentMethodWriter;
-        $this->shopRepository = $shopRepository;
     }
 
     public function __invoke(PaymentMeanCollection $paymentMeans): PaymentMeanCollection
