301db0dcca5e7be8d190e4ae12f4e910c4fedd2a
Filippe Bortels <filippe.bortels@phpro.be>
2021-11-26 18:39:55 +0100

    ASW-387 update unit test

--- tests/Unit/Models/Payment/PaymentMeanTest.php
+++ tests/Unit/Models/Payment/PaymentMeanTest.php
@@ -7,6 +7,7 @@ namespace AdyenPayment\Tests\Unit\Models\Payment;
 use AdyenPayment\Models\Enum\PaymentMethod\SourceType;
 use AdyenPayment\Models\Payment\PaymentMean;
 use PHPUnit\Framework\TestCase;
+use Shopware\Bundle\StoreFrontBundle\Struct\Attribute;
 
 final class PaymentMeanTest extends TestCase
 {
@@ -17,6 +18,10 @@ final class PaymentMeanTest extends TestCase
         $this->paymentMean = PaymentMean::createFromShopwareArray([
             'id' => '15',
             'source' => '1425514',
+            'attribute' => new Attribute([
+                'adyen_type' => 'adyen-type',
+                'adyen_stored_method_id' => 'stored payment method id',
+            ]),
         ]);
     }
 
@@ -41,6 +46,10 @@ final class PaymentMeanTest extends TestCase
         $this->assertEquals([
             'id' => '15',
             'source' => '1425514',
+            'attribute' => new Attribute([
+                'adyen_type' => 'adyen-type',
+                'adyen_stored_method_id' => 'stored payment method id',
+            ]),
         ], $this->paymentMean->getRaw());
     }
 
@@ -61,5 +70,40 @@ final class PaymentMeanTest extends TestCase
     public function it_can_retrieve_a_value_with_fallback(): void
     {
         $this->assertEquals('fallback', $this->paymentMean->getValue('non-existent', 'fallback'));
+    }
+
+    /** @test */
+    public function it_can_retrieve_an_attribute(): void
+    {
+        $this->assertEquals(new Attribute([
+            'adyen_type' => 'adyen-type',
+            'adyen_stored_method_id' => 'stored payment method id',
+        ]), $this->paymentMean->getAttribute());
+    }
+
+    /** @test */
+    public function it_can_retrieve_attribute_adyen_type(): void
+    {
+        $this->assertEquals('adyen-type', $this->paymentMean->getAdyenType());
+    }
+
+    /** @test */
+    public function it_can_retrieve_default_attribute_adyen_type(): void
+    {
+        $paymentMean = PaymentMean::createFromShopwareArray(['source' => null]);
+        $this->assertEquals('', $paymentMean->getAdyenType());
+    }
+
+    /** @test */
+    public function it_can_retrieve_attribute_adyen_stored_method_id(): void
+    {
+        $this->assertEquals('stored payment method id', $this->paymentMean->getAdyenStoredMethodId());
+    }
+
+    /** @test */
+    public function it_can_retrieve_default_attribute_adyen_stored_method_id(): void
+    {
+        $paymentMean = PaymentMean::createFromShopwareArray(['source' => null]);
+        $this->assertEquals('', $paymentMean->getAdyenStoredMethodId());
     }
 }
