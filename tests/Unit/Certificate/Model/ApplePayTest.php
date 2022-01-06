3e60ea8841799058d44114793085e9febace1e0b
davebueds <david.bueds@meteor.be>
2022-01-06 14:23:52 +0100

    ASW-429 add apple pay unit test

---
+++ tests/Unit/Certificate/Model/ApplePayTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Tests\Unit\Certificate\Model;
+
+use AdyenPayment\Certificate\Model\ApplePay;
+use Dexis\Erp\Model\Offer\OfferStatus;
+use PHPUnit\Framework\TestCase;
+
+class ApplePayTest extends TestCase
+{
+    private ApplePay $applePay;
+
+    protected function setUp(): void
+    {
+        $this->applePay = ApplePay::create('certificate string');
+    }
+
+    /** @test */
+    public function it_contains_a_certificate_string(): void
+    {
+        $this->assertEquals('certificate string', $this->applePay->certificateString());
+    }
+
+    /** @test */
+    public function it_can_be_constructed_by_load(): void
+    {
+        $applePay = ApplePay::create($certificateString = 'test');
+        $this->assertEquals($certificateString, $applePay->certificateString());
+    }
+}
