d36f5976278003e7f275eb1f3abb6d3df414c61d
davebueds <david.bueds@meteor.be>
2022-01-14 21:26:07 +0100

    ASW-430 make apple pay certificate available on endpoint using cronjob and live

--- Certificate/Subscriber/Backend/ApplePayCertificateUrlRewriterSubscriber.php
+++ Certificate/Subscriber/Backend/ApplePayCertificateUrlRewriterSubscriber.php
@@ -10,8 +10,9 @@ final class ApplePayCertificateUrlRewriterSubscriber implements SubscriberInterf
 {
     private \Shopware_Components_Modules $modules;
 
-    public function __construct(\Shopware_Components_Modules $modules) {
-       $this->modules = $modules;
+    public function __construct(\Shopware_Components_Modules $modules)
+    {
+        $this->modules = $modules;
     }
 
     public static function getSubscribedEvents(): array
@@ -19,8 +20,6 @@ final class ApplePayCertificateUrlRewriterSubscriber implements SubscriberInterf
         return [
             'Shopware_CronJob_RefreshSeoIndex_CreateRewriteTable' => 'createApplePayCertificateRewriteTable',
             'sRewriteTable::sCreateRewriteTable::after' => 'createApplePayCertificateRewriteTable',
-            'Enlight_Controller_Action_PostDispatch_Backend_Performance' => 'loadPerformanceExtension',
-            'Shopware_Controllers_Seo_filterCounts' => 'addApplePayCertificateCount',
         ];
     }
 
@@ -32,42 +31,5 @@ final class ApplePayCertificateUrlRewriterSubscriber implements SubscriberInterf
             'sViewport=applepaycertificate',
             'well-known/apple-developer-merchantid-domain-association'
         );
-    }
-
-    public function loadPerformanceExtension(\Enlight_Controller_ActionEventArgs $args)
-    {
-        $subject = $args->getSubject();
-        $request = $subject->Request();
-
-        if ($request->getActionName() !== 'load') {
-            return;
-        }
-
-        $subject->View()->addTemplateDir($this->getPath() . '/Resources/views/');
-        $subject->View()->extendsTemplate('backend/performance/view/applepaycertificate.js');
-    }
-
-    public function addApplePayCertificateCount(\Enlight_Event_EventArgs $args)
-    {
-        $counts = $args->getReturn();
-
-        // Currently, there's only a single URL to be generated for each shop,
-        // so we'll just return a static 1.
-        $counts['applepaycertificate'] = 1;
-
-        return $counts;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getPath()
-    {
-        if (null === $this->path) {
-            $reflected = new \ReflectionObject($this);
-            $this->path = \dirname($reflected->getFileName());
-        }
-
-        return $this->path;
     }
 }
