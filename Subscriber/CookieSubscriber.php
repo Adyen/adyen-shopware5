961d336e5108faf992382946a7682b94eba3589e
Sava Gavrić <sava.gavric@logeecom.com>
2022-09-21 12:43:16 +0200

    Revert "Remove custom cookie subscriber"
    This reverts commit 8eecbc7ac4875eb956692f4ca8967947543b92f4.

---
+++ Subscriber/CookieSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdyenPayment\Subscriber;
+
+use Enlight\Event\SubscriberInterface;
+use Shopware\Bundle\CookieBundle\CookieCollection;
+use Shopware\Bundle\CookieBundle\Structs\CookieGroupStruct;
+use Shopware\Bundle\CookieBundle\Structs\CookieStruct;
+
+final class CookieSubscriber implements SubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'CookieCollector_Collect_Cookies' => 'addAdyenCookie',
+        ];
+    }
+
+    public function addAdyenCookie(): CookieCollection
+    {
+        return new CookieCollection([
+            new CookieStruct(
+                'comfort',
+                '/^adyen/',
+                'Adyen Cookies',
+                CookieGroupStruct::TECHNICAL
+            ),
+        ]);
+    }
+}
