3154ca137ef4ea51360225d57a6a7e0cb3b03432
Tamara <tamara@example.com>
2023-12-13 10:37:19 +0100

    Revert "Test"
    This reverts commit f6cfc8596a26f0ef8c0817f9b4bdae90ab27800d.

--- E2ETest/Services/CreateWebhooksDataService.php
+++ E2ETest/Services/CreateWebhooksDataService.php
@@ -82,7 +82,7 @@ class CreateWebhooksDataService extends BaseCreateSeedDataService
         if ($webhookConfig) {
             $authData['username'] = $webhookConfig->getUsername();
             $authData['password'] = $webhookConfig->getPassword();
-            $authData['hmac'] = $webhookConfig->getHmac() ?? 'test';
+            $authData['hmac'] = $webhookConfig->getHmac();
         }
 
         return $authData;
